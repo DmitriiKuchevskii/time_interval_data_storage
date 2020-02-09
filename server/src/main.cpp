50233f90953f83011da9cf51cc7a51498b04547d
Dmitrii Kuchevskii <dmitrii@kuchevskii.com>
2020-02-09 18:17:27 +0300

    Added port command line arg.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cpp-optparse/OptionParser.h>
 #include "server/asio/service.h"
 #include "system/cpu.h"
 #include "MultiSessionServer.h"
@@ -5,10 +6,15 @@
 
 int main(int argc, char** argv)
 {
+    auto parser = optparse::OptionParser().version("1.0.0.0");
+    parser.add_option("-p", "--port").dest("port").action("store").type("int").set_default(1111).help("Server port. Default: %default");
+    optparse::Values options = parser.parse_args(argc, argv);
+
     auto service = std::make_shared<Service>(std::thread::hardware_concurrency(), true);
-    int port = 1111;
+    int port = options.get("port");
     auto server = make_multi_session_server<TimeIntervalSumSession<int64_t>>(service, port, 60 * 1000);
     std::cout << "Listening on port: " << server->port() << "\nPress any key to terminate the server.\n";
     server->Start();
     std::cin.get();
+    std::cout << "ALL DONE!";
 }
